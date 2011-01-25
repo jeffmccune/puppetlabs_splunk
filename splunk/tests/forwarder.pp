1635e11e24fcb55538f6040954f44c553f98b950
Cody Herriges <c.a.herriges@gmail.com>
2011-01-25 14:30:45 -0800

    Extra /

--- splunk/tests/forwarder.pp
+++ splunk/tests/forwarder.pp
@@ -3,7 +3,7 @@ class { "splunk::users":
 }
 
 class { "splunk::package":
-  pkg_base => "http://tia.cat.pdx.edu/",
+  pkg_base => "http://tia.cat.pdx.edu",
   pkg_file => "splunk-4.1.6-89596-linux-2.6-x86_64.rpm",
   repo     => false,
 }
