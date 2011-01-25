692cea598904c215fb4d7afe1a54b015f014ca54
Cody Herriges <c.a.herriges@gmail.com>
2011-01-25 14:35:50 -0800

    Wrong atribute

--- splunk/tests/forwarder.pp
+++ splunk/tests/forwarder.pp
@@ -5,7 +5,7 @@ class { "splunk::users":
 class { "splunk::package":
   pkg_base => "http://tia.cat.pdx.edu",
   pkg_file => "splunk-4.1.6-89596-linux-2.6-x86_64.rpm",
-  repo     => false,
+  has_repo => false,
 }
 
 class { "splunk::service": }
