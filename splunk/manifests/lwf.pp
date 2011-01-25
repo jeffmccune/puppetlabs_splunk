f0140dffb27042df3d73e65f021249af1a212c93
Cody Herriges <c.a.herriges@gmail.com>
2011-01-25 14:10:42 -0800

    To easily flip on the LWF application.

---
+++ splunk/manifests/lwf.pp
@@ -0,0 +1,41 @@
+# Define: splunk::lwf
+#
+#   Turns on the Splunk included Light Weight
+#   Forwarder application.
+#
+#   Cody Herriges <cody@puppetlabs.com>
+#   2011-1-25
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class splunk::lwf(
+  $enable    = true,
+  $basepath  = $splunk::users::home
+  ) {
+
+  if ! ($enable == true or $enable == false) {
+    fail("enable must be true or false")
+  }
+
+  file { "lwf_local":
+      path   => "${basepath}/etc/apps/SplunkLightForwarder/local",
+      ensure => directory,
+      owner  => "splunk",
+      group  => "splunk",
+      mode   => "0755",
+  }
+
+  file { "lwf_appconf":
+      path => "${basepath}/etc/apps/SplunkLightForwarder/local/app.conf",
+      content => template("splunk/lwfappconf.erb"),
+      owner  => "splunk",
+      group  => "splunk",
+      mode   => "0644";
+  }
+}
