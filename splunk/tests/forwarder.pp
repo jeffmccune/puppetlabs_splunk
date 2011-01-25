524847ece5c61ba18aa38768e70b7bea77aae236
Cody Herriges <c.a.herriges@gmail.com>
2011-01-25 13:22:22 -0800

    Added a forwarder.pp for testing.

---
+++ splunk/tests/forwarder.pp
@@ -0,0 +1,16 @@
+class { "splunk::users":
+  virtual => false
+}
+
+class { "splunk::package": }
+
+class { "splunk::service": }
+
+class { "splunk": }
+class { "splunk::app": }
+class { "splunk::lwf": }
+class { "splunk::inputs": }
+splunk::inputs::fragment {
+  'yum':
+    target => '/var/log/yum.log',
+}
