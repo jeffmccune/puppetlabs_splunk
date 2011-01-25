a8af2ed5166ad1f8f85d610ed4a42eb902f8ad77
Cody Herriges <c.a.herriges@gmail.com>
2011-01-24 16:29:00 -0800

    URLs that conform to splunk's website.

--- splunk/manifests/package.pp
+++ splunk/manifests/package.pp
@@ -16,48 +16,46 @@
 # Sample Usage:
 #
 class splunk::package(
-  $pkg_name = "splunk",
-	$pkg_base = "http://www.puppetlabs.lan/yum/splunk",
-	$pkg_file = "splunk-4.1.6-89596-linux-2.6-x86_64.rpm",
-	$version  = "4.1.6-89596",
-	$has_repo = false,
-	$ensure   = "present"
+  $pkg_name   = "splunk",
+  $pkg_file   = "splunk-4.1.6-89596-linux-2.6-x86_64.rpm",
+  $version    = "4.1.6",
+  $has_repo   = false,
+  $ensure     = "present"
 ) {
   # JJM Note, this should break out to platform specific
   # secondary classes.  They should NOT be subclasses.
 
-	if ! ($ensure == "absent" or $ensure == "present") {
+  if ! ($ensure == "absent" or $ensure == "present") {
     fail("ensure param must be 'absent' or 'present'")
-	}
+  }
 
   # JJM Ideally, the customer would have a local yum repository,
   # but this is often not the case, so we need to install
   # from URL.
 
   # JJM FIXME, this needs to not assume RPM.
-	if ($has_repo == false) {
+  if ($has_repo == false) {
     $provider   = "rpm"
-    $pkg_source = "${pkg_base}/${pkg_file}"
+    $pkg_source = "http://www.splunk.com/index.php/download_track?file=${version}/linux/${pkg_file}&ac=&wget=true&name=wget&typed=releases"
   } elsif ($has_repo == true) {
     $provider   = "yum"
     $pkg_source = undef
-	} else {
-		fail("has_repo param must be true or false")
-	}
+  } else {
+    fail("has_repo param must be true or false")
+  }
 
-# End sanity checking
-# Begin resource declarations
+  # End sanity checking
+  # Begin resource declarations
 
-	package { "splunk":
-	  name     => $pkg_name,
-	  ensure   => $ensure,
-	  provider => $provider,
-		source   => $pkg_source,
-	}
+  package { "splunk":
+    name     => $pkg_name,
+    ensure   => $ensure,
+    provider => $provider,
+    source   => $pkg_source,
+  }
 
   # Relationships
   if ($ensure == "present") {
     User["splunk"] -> Package["splunk"]
   }
-
 }
